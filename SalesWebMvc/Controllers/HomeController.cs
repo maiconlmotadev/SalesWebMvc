dd32d9dd8ad968ec7d458acec3c374f75966a808
Maicon Mota <maiconlmotadev@hotmail.com>
2023-05-05 16:33:37 +0100

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web mvc app.";
+            ViewData["email"] = "maiconlmotadev@hotmail.com";
 
             return View();
         }
